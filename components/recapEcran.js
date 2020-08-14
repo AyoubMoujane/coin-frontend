f40a33ad1a31851a6eecb9bb7288bcdef723fff2
AyoubMoujane <moujane.ayoub@hotmail.com>
2020-08-14 17:48:11 +0200

    Affichage des infos de transactions

--- components/recapEcran.js
+++ components/recapEcran.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import { connect } from "react-redux";
 
-export default function recapEcran() {
-  return (
-    <View>
-      <Text>Ecran recap des conso et offres</Text>
-    </View>
-  );
+const mapStateToProps = (state) => {
+  return {
+    utilisateur: state.auth.user,
+  };
+};
+
+function recapEcran({ utilisateur }) {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+    fetch(
+      `http://192.168.1.26:73/transactions/auteur/${utilisateur.idUtilisateur}`,
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          console.log(result);
+          setTransactions(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <Text>Error: {error.message}</Text>;
+  } else if (!isLoaded) {
+    return <Text>Loading...</Text>;
+  } else {
+    return (
+      <View>
+        {transactions.map((transaction) => (
+          <Text key={transaction.idTransaction}>
+            {transaction.identifiantAuteur} -- {transaction.Montant} --D{" "}
+            {transaction.identifiantDestinataire}
+          </Text>
+        ))}
+      </View>
+    );
+  }
 }
+
+export default connect(mapStateToProps)(recapEcran);
