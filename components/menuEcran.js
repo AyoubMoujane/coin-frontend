6203041a3aa9536094f39dcfd5b601c9f44d4c19
AyoubMoujane <moujane.ayoub@hotmail.com>
2020-09-03 21:35:37 +0200

    Ajout du refresh qui fetch le solde actel

--- components/menuEcran.js
+++ components/menuEcran.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  ScrollView,
+  RefreshControl,
+  StyleSheet,
+} from "react-native";
 import { connect } from "react-redux";
 import { logOut } from "../redux/actions/authActions";
 import { API_HOST } from "../environment/dev.env";
@@ -15,12 +22,36 @@ const mapDispatchToProps = (dispatch) => {
     logOut: () => dispatch(logOut()),
   };
 };
+
+// Pour le rafraichissement
+
+const wait = (timeout) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+};
+
 function menuEcran({ navigation, utilisateur, logOut }) {
   const [solde, setSolde] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [flashMessage, setFlashMessage] = useState("");
 
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+
+    wait(500).then(() => {
+      getSolde();
+      setRefreshing(false);
+    });
+  }, []);
+
   useEffect(() => {
+    getSolde();
+  }, []);
+
+  const getSolde = () => {
     setIsLoading(true);
     fetch(`http://${API_HOST}/utilisateurs/+${utilisateur.idUtilisateur}`, {
       method: "GET",
@@ -34,7 +65,7 @@ function menuEcran({ navigation, utilisateur, logOut }) {
       .catch((error) => {
         setIsLoading(false);
       });
-  }, []);
+  };
 
   function handleErrors(response) {
     if (!response.ok) {
@@ -69,21 +100,35 @@ function menuEcran({ navigation, utilisateur, logOut }) {
   };
 
   return (
-    <View>
-      <Text>Ecran menu principal</Text>
-      <Text>Identifiant : {utilisateur.identifiant}</Text>
-      <Text>Solde : {isLoading ? "Chargement..." : solde}</Text>
-      <Text>Groupe : {utilisateur.libelleGroupe}</Text>
-      <Text>
-        Seuil : {utilisateur.Seuil ? utilisateur.Seuil : "Aucun plafond"}
-      </Text>
+    <ScrollView
+      contentContainerStyle={styles.scrollView}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
+      <View>
+        <Text>Ecran menu principal</Text>
+        <Text>Identifiant : {utilisateur.identifiant}</Text>
+        <Text>Solde : {isLoading ? "Chargement..." : solde}</Text>
+        <Text>Groupe : {utilisateur.libelleGroupe}</Text>
+        <Text>
+          Seuil : {utilisateur.Seuil ? utilisateur.Seuil : "Aucun plafond"}
+        </Text>
 
-      <Button title="Consommer" onPress={pressConsommerHandler} />
-      <Button title="Offrir" onPress={pressOffrirHandler} />
-      <Button title="Recapitulatif" onPress={pressRecapHandler} />
-      <Button title="Deconnexion" onPress={pressDeconnexionHandler} />
-    </View>
+        <Button title="Consommer" onPress={pressConsommerHandler} />
+        <Button title="Offrir" onPress={pressOffrirHandler} />
+        <Button title="Recapitulatif" onPress={pressRecapHandler} />
+        <Button title="Deconnexion" onPress={pressDeconnexionHandler} />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(menuEcran);
