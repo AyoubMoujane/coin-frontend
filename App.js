e615607f9336cb93fdac243b431cff0925963bc4
AyoubMoujane <moujane.ayoub@hotmail.com>
2020-09-03 21:35:09 +0200

    Ajout du safeAreaView sur l'app globale

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  SafeAreaView,
+} from "react-native";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Constants from "expo-constants";
 
 import AuthLoading from "./components/authLoading";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <AuthLoading />
-    </Provider>
+    <SafeAreaView style={styles.container}>
+      <Provider store={store}>
+        <AuthLoading />
+      </Provider>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
