3696cf85c9014b9cb39d0c5bdc76894155863033
AyoubMoujane <moujane.ayoub@hotmail.com>
2020-09-04 18:59:04 +0200

    ajout scrollView et recuperation des transactions recues

--- components/recapEcran.js
+++ components/recapEcran.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet, SafeAreaView } from "react-native";
 import { connect } from "react-redux";
 import { API_HOST } from "../environment/dev.env";
 import moment from "moment";
@@ -13,32 +13,68 @@ const mapStateToProps = (state) => {
 function recapEcran({ utilisateur }) {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [transactions, setTransactions] = useState([]);
-  useEffect(() => {
-    fetch(
-      `http://${API_HOST}/transactions/auteur/${utilisateur.idUtilisateur}`,
-      {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      }
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          const sortedResults = result.sort(
-            (a, b) => a.Date_creation < b.Date_creation
-          );
-          setTransactions(sortedResults);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
+  const [transactionsA, setTransactionsA] = useState([]);
+  const [transactionsD, setTransactionsD] = useState([]);
+
+  const fetchTransactionA = () => {
+    return new Promise((resolve, reject) => {
+      fetch(
+        `http://${API_HOST}/transactions/auteur/${utilisateur.idUtilisateur}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
         }
-      );
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            const sortedResults = result.sort(
+              (a, b) => a.Date_creation < b.Date_creation
+            );
+            setTransactionsA(sortedResults);
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+    });
+  };
+
+  const fetchTransactionD = () => {
+    return new Promise((resolve, reject) => {
+      fetch(
+        `http://${API_HOST}/transactions/destinataire/${utilisateur.idUtilisateur}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            const sortedResults = result.sort(
+              (a, b) => a.Date_creation < b.Date_creation
+            );
+            setTransactionsD(sortedResults);
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+    });
+  };
+
+  useEffect(() => {
+    setIsLoaded(false);
+    Promise.all([fetchTransactionA(), fetchTransactionD()])
+      .then(() => {
+        setIsLoading(true);
+      })
+      .catch((err) => {
+        setIsLoaded(true);
+      });
   }, []);
 
   if (error) {
@@ -47,17 +83,43 @@ function recapEcran({ utilisateur }) {
     return <Text>Loading...</Text>;
   } else {
     return (
-      <View>
-        {transactions.map((transaction) => (
-          <Text key={transaction.idTransaction}>
-            {transaction.identifiantAuteur} -- {transaction.Montant} --D{" "}
-            {transaction.identifiantDestinataire}{" "}
-            {moment(transaction.Date_creation).format("llll")}
-          </Text>
-        ))}
+      <View style={styles.container}>
+        <View style={styles.halfContainer}>
+          <Text>Transactions envoyees</Text>
+          <ScrollView>
+            {transactionsA.map((transaction) => (
+              <Text key={transaction.idTransaction}>
+                {transaction.identifiantAuteur} -- {transaction.Montant} --D{" "}
+                {transaction.identifiantDestinataire}{" "}
+                {moment(transaction.Date_creation).format("llll")}
+              </Text>
+            ))}
+          </ScrollView>
+        </View>
+        <View style={styles.halfContainer}>
+          <Text>Transactions recues</Text>
+          <ScrollView>
+            {transactionsD.map((transaction) => (
+              <Text key={transaction.idTransaction}>
+                {transaction.identifiantAuteur} -- {transaction.Montant} --D{" "}
+                {transaction.identifiantDestinataire}{" "}
+                {moment(transaction.Date_creation).format("llll")}
+              </Text>
+            ))}
+          </ScrollView>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  halfContainer: {
+    flex: 1 / 2,
+  },
+});
 
 export default connect(mapStateToProps)(recapEcran);
