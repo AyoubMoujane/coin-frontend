80c056934bb9746a3fdc9673096d2329d8697aa0
AyoubMoujane <moujane.ayoub@hotmail.com>
2020-10-07 21:00:01 +0200

    Ajout l'image de chargement lors de l'ouverture de l'app

--- App.js
+++ App.js
@@ -12,17 +12,44 @@ import {
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Constants from "expo-constants";
+import { Asset } from "expo-asset";
+import { AppLoading } from "expo";
 
 import AuthLoading from "./components/authLoading";
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Provider store={store}>
-        <AuthLoading />
-      </Provider>
-    </SafeAreaView>
-  );
+export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Provider store={store}>
+          <AuthLoading />
+        </Provider>
+      </SafeAreaView>
+    );
+  }
+
+  async _cacheResourcesAsync() {
+    const images = [require("./assets/logo-coin.png")];
+
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    return Promise.all(cacheImages);
+  }
 }
 
 const styles = StyleSheet.create({
